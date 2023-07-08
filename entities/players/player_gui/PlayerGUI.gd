2e1918ec5eac2f14c62f66eefab5560bd29cd629
Ryan <ryanwp526@gmail.com>
2023-07-08 19:04:21 +1000

    player health bars complete

--- entities/players/player_gui/PlayerGUI.gd
+++ entities/players/player_gui/PlayerGUI.gd
@@ -3,7 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$HealthBar.value = 100
+	$HealthBarHolder.get_node("HealthBar").value = 100
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +12,4 @@ func _process(delta):
 
 func health_changed(new_health, difference):
 	var max_health = get_parent().max_health
-	$HealthBar.value = round((new_health/max_health) * 100)
+	$HealthBarHolder.get_node("HealthBar").value = round((float(new_health)/max_health) * 100)
