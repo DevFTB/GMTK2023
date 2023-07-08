00cf279129192c4e30e21ec780637fbdbb481d12
Ryan <ryanwp526@gmail.com>
2023-07-08 21:03:48 +1000

    Player status effects complete

--- entities/players/Player.gd
+++ entities/players/Player.gd
@@ -8,6 +8,12 @@ var health: int
 
 var slowed = false
 var stunned = false
+var slow_factor = 0.5
+
+var status_sprite_mappings = {
+	"stunned": 0,
+	"slowed": 1
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,11 +36,42 @@ func heal(heal: int):
 	$PlayerGUI.health_changed(health, heal)
 	print(name + " healed for  " + str(heal))
 
+func update_status_sprite():
+	$StatusEffect.visible = true
+	if stunned:
+		$StatusEffect.frame = status_sprite_mappings["stunned"]
+	elif slowed:
+		$StatusEffect.frame = status_sprite_mappings["slowed"]
+	else:
+		$StatusEffect.visible = false
+
+func apply_stun(time=1):
+	stunned = true
+	var stun_timer = $StatusEffect.get_node("StunTimer")
+	if stun_timer.time_left < time:
+		stun_timer.start(time)
+	update_status_sprite()
+	
+func apply_slow(time=1):
+	slowed = true
+	var slow_timer = $StatusEffect.get_node("SlowTimer")
+	if slow_timer.time_left < time:
+		slow_timer.start(time)
+	update_status_sprite()
+
 func move(loc, delta):
-	var loc_diff = loc - global_position
-	var vel_vec = loc_diff if loc_diff.length() < 1 else loc_diff.normalized()
-	velocity = speed * vel_vec
-	move_and_slide()
+	if not stunned:
+		var loc_diff = loc - global_position
+		var vel_vec = loc_diff if loc_diff.length() < 1 else loc_diff.normalized()
+		if slowed:
+			vel_vec *= slow_factor
+		velocity = speed * vel_vec
+		# TODO: sometimes you can drag players around as the boss, maybe should fix lol
+		move_and_slide()
+
+func do_action(action: Node2D, target):
+	if not stunned:
+		action.do(target)
 	
 #	if (loc - global_position).length() <= speed * delta:
 #		global_position = loc
@@ -43,3 +80,13 @@ func move(loc, delta):
 	
 func die():
 	pass
+
+
+func _on_stun_timer_timeout():
+	stunned = false
+	update_status_sprite()
+
+
+func _on_slow_timer_timeout():
+	slowed = false
+	update_status_sprite()
