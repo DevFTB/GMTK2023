9de45b885a504915c975f7b5fd2b41bae5f8c0ff
Aditha Doratiyawa <indegamedev@gmail.com>
2023-07-09 08:39:44 +1000

    better beat bar

--- level/beat/BeatGUI.gd
+++ level/beat/BeatGUI.gd
@@ -6,9 +6,12 @@ const BossState = preload("res://entities/boss/BossState.gd")
 @export var beat_manager : BeatManager
 
 
-@onready var lbl = $LeftBeatLabel
-@onready var rbl = $RightBeatLabel
-@onready var fg_color_rect = $Bar/ForegroundColourRect
+@onready var lbl = $BarArea/LeftBeatLabel
+@onready var rbl = $BarArea/RightBeatLabel
+@onready var fg_color_rect = $BarArea/Bar/ForegroundColourRect
+@onready var ball = $BarArea/Bar/Ball
+@onready var left_warner = $LeftWarner
+@onready var right_warner = $RightWarner
 
 var alc : ComboLifecycle.ActionLifecycle
 
@@ -25,11 +28,11 @@ func _ready():
 func _process(delta):
 	if is_going_right():
 		# go right
-		$Ball.position.x = $Bar.size.x * (fmod(beat_manager.timer, beat_manager.time_per_beat) / beat_manager.time_per_beat) - $Ball.size.x / 2
+		ball.position.x = $BarArea/Bar.size.x * (fmod(beat_manager.timer, beat_manager.time_per_beat) / beat_manager.time_per_beat) - ball.size.x / 2
 		pass
 	else:
 		# go left
-		$Ball.position.x = $Bar.size.x * (1 - (fmod(beat_manager.timer, beat_manager.time_per_beat) / beat_manager.time_per_beat)) - $Ball.size.x / 2
+		ball.position.x = $BarArea/Bar.size.x * (1 - (fmod(beat_manager.timer, beat_manager.time_per_beat) / beat_manager.time_per_beat)) - ball.size.x / 2
 		pass
 
 func _on_beat(beat_number):
@@ -54,7 +57,7 @@ func _on_hit_action(action: BossAction, strength: float):
 	if not combo.action_queue.is_empty():
 		set_alc(combo.action_queue.front())
 		print(alc.action.action_name, " giss")
-	$Control/HitLabel.display_hit_text(strength)
+	$BarArea/Control/HitLabel.display_hit_text(strength)
 	
 func set_alc(new_alc):
 	alc = new_alc
@@ -62,8 +65,17 @@ func set_alc(new_alc):
 		if alc.action.is_hold_action:
 			alc.started.connect(func(): fg_color_rect.active = true)
 			alc.hit.connect(func(x): fg_color_rect.active = false)
-	_update_gui()
+			assign_to_warner(alc.start_beat, true, true)
 	
+		assign_to_warner(alc.hit_beat, not alc.action.is_hold_action, alc.action.is_hold_action)
+	_update_gui()
+
+func assign_to_warner(beat: int, is_press, is_hold):
+	if not beat % 2 == 0:
+		$RightWarner.submit_input(beat, is_press, is_hold)
+	else:
+		$LeftWarner.submit_input(beat, is_press, is_hold)
+
 func _update_gui():
 	var lbl_text = ""
 	var rbl_text = ""
@@ -72,7 +84,6 @@ func _update_gui():
 		if alc.action.is_hold_action:
 			if alc.start_beat == beat_manager.beat_number + 1:
 				set_text("Start Hold!")
-				
 			
 			if alc.hit_beat == beat_manager.beat_number + 1:
 				set_text("Release!")
@@ -81,7 +92,7 @@ func _update_gui():
 				set_text("Hit!")
 		else:
 			set_text("")
-
+	
 func clear_text():
 	alc = null
 	fg_color_rect.active = false
