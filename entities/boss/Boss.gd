fe49b76ba9ab3831f7ae4a955352a92b221812e8
Ryan <ryanwp526@gmail.com>
2023-07-09 10:17:42 +1000

    BOSS EPIC NAME GENERATOR COMPLETE

--- entities/boss/Boss.gd
+++ entities/boss/Boss.gd
@@ -7,12 +7,17 @@ signal started_combo(lc: ComboLifecycle)
 @export var combos : Array[Combo] = []
 @export var max_health = 20
 
+@export var prefix_path = "res://entities/boss/boss_name/prefixes.txt"
+@export var suffix_path = "res://entities/boss/boss_name/suffixes.txt"
+
 var health
 var movement_dir = Vector2.ZERO
 var active_combo : Node2D
+var names
 
 func _ready():
 	health = max_health
+	names = generate_boss_names(60)
 
 func _process(delta):
 	velocity = movement_dir * movement_speed
@@ -34,3 +39,34 @@ func activate_combo(index: int) -> void:
 func take_damage(damage: int):
 	health -= damage
 	print("Boss took %s damage. Now at %s HP" % [damage, health])
+	
+func generate_boss_names(n=50):
+	var prefixes = read_file(prefix_path).split("\n")
+	var suffixes = read_file(suffix_path).split("\n")
+	
+	# for some reason godot forces a newline at the end of the text file...
+	prefixes = prefixes.slice(0, -1)
+	suffixes = suffixes.slice(0, -1)
+	
+	var modifiers = {}
+	for p in prefixes:
+		modifiers[p] = "Prefix"
+	for s in suffixes:
+		modifiers[s] = "Suffix"
+
+	var names = []
+	var name = "Boss"
+	
+	for i in range(n):
+		names.append(name)
+		var new = modifiers.keys().pick_random()
+		if modifiers[new] == "Prefix":
+			name = new + " "  + name
+		elif modifiers[new] == "Suffix":
+			name = name + " " + new
+		modifiers.erase(new)
+	
+	return names
+# todo make sure this exports correctly!! otherwise just hardcode it in
+func read_file(path):
+	return FileAccess.open(path, FileAccess.READ).get_as_text()
