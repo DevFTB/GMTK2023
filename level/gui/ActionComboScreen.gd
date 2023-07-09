ad8d0c89021203b8d7ab8e44e4275ef75092eba0
Ryan <ryanwp526@gmail.com>
2023-07-09 20:23:41 +1000

    Hook up money label to the gold

--- level/gui/ActionComboScreen.gd
+++ level/gui/ActionComboScreen.gd
@@ -5,6 +5,7 @@ extends Control
 func _ready():
 	var start_button = get_node("HBoxContainer/VBoxContainer/Control/HBoxContainer/Control2/MarginContainer/StartButton")
 	start_button.button_down.connect(get_parent().get_parent()._on_combo_screen_start_button_pressed)
+	boss_stats.gold_changed.connect(_on_gold_changed)
 
 func _on_action_detail_bought(action: BossAction):
 	boss_stats.spend_gold(action.gold_cost)
@@ -16,3 +17,6 @@ func _on_action_detail_upgraded(action: BossAction):
 	boss_stats.spend_gold(action.get_upgrade_cost())
 	action.upgrade()
 	pass # Replace with function body.
+
+func _on_gold_changed():
+	get_node("HBoxContainer/VBoxContainer/Control/HBoxContainer/Control/MarginContainer/HBoxContainer/Money").text = str(boss_stats.gold)
